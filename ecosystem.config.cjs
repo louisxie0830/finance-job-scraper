4a391effd3533fa18cee6d48f682c3d518da9851
Nil Xie <bfgh2678@gmail.com>
2024-07-08 23:35:35 +0800

    fix: update pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,13 +1,16 @@
 module.exports = {
-  apps: [{
-    name: 'finance-job-scraper',
-    script: './src/app.js',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'production'
-    }
-  }]
+  apps: [
+    {
+      name: 'finance-job-scraper',
+      script: './src/app.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
 };

43ad008187f872b6618840149c21703ec92f06fd
Nil Xie <bfgh2678@gmail.com>
2024-11-17 22:17:41 +0800

    fix: update start script

--- start.sh
+++ start.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# echo "Starting dbus service..."
-# dbus-uuidgen > /etc/machine-id
-# service dbus start
+echo "Starting dbus service..."
+dbus-uuidgen > /etc/machine-id
+service dbus start
 
-# echo "Starting Xvfb..."
-# Xvfb :99 -screen 0 1024x768x24 &
-
-# export DISPLAY=:99
+echo "Starting Xvfb..."
+Xvfb :99 -screen 0 1024x768x24 &
+export DISPLAY=:99
 
 echo "Starting application..."
 npm run run-prod

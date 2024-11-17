65d8e83845c8a391a8af5adb8fa406e999fd7e6c
Nil Xie <bfgh2678@gmail.com>
2024-11-17 21:28:42 +0800

    fix: update pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -12,6 +12,10 @@ module.exports = {
       autorestart: true,
       watch: false,
       max_memory_restart: `${totalMemoryGB}G`,
+      error_file: "./logs/error.log",
+      out_file: "./logs/output.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      cron_restart: "0 2 * * *",
       env: {
         NODE_ENV: 'production',
       },
